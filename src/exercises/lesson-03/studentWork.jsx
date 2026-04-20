494861edc4f4b7372d55ed9033468b874f1a92ab
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-20 21:37:01 +0100

    Fix Bug #2 — Mutated State

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,11 +1,13 @@
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
 import BugEffectLoop from './BugEffectLoop';
+import BugMutatedState from './BugMutatedState';
 
 export default function StudentWork() {
   return (
     <div>
       <BugEffectLoop />
+      <BugMutatedState />
     </div>
   );
 }
