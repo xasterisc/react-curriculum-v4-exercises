ecbb28b15c7fe7f19cf98552b461dcc79c4b7095
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-20 21:15:10 +0100

    Fix Bug #1 — Effect Issue

--- src/exercises/lesson-03/studentWork.jsx
+++ src/exercises/lesson-03/studentWork.jsx
@@ -1,12 +1,11 @@
 //Lesson-03 Component Lifecycle, Hooks, State, and Props
 //Exercise: React Bug Hunt – Fix the broken components in this folder
-//Impport components here
+import BugEffectLoop from './BugEffectLoop';
 
 export default function StudentWork() {
   return (
     <div>
-      {/* add components here */}
-      <p>Student output will go here</p>
+      <BugEffectLoop />
     </div>
   );
 }
