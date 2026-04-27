80afe077aea0538b1e3c95f120dc3a62cc81c463
Mohamed Zouari <62741160+xasterisc@users.noreply.github.com>
2026-04-27 21:25:35 +0100

    Imperatively re-focus the input when the button is clicked.

--- src/exercises/lesson-04/FillRefFocus.jsx
+++ src/exercises/lesson-04/FillRefFocus.jsx
@@ -1,13 +1,22 @@
 // TOPIC: Correct useRef usage to control DOM elements
 // TASK: Implement focusing an input field when the button is clicked.
+
+import { useRef } from 'react';
+
 export default function FillRefFocus() {
-  function focusInput() {}
+  const inputRef = useRef(null);
+
+  function focusInput() {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }
 
   return (
     <div>
       <h2>useRef: Focusing an Input</h2>
 
-      <input type="text" placeholder="Type here..." />
+      <input ref={inputRef} type="text" placeholder="Type here..." />
 
       <button onClick={focusInput}>Focus Input</button>
     </div>
